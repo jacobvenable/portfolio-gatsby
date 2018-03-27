4a49a328ccb7e24e0cbbe03df8d3f403046dbc1e
jacobvenable <jacob.venable@gmail.com>
2018-03-26 21:19:08 -0600

    Install gatsby postcss-sass plugin and autoprefixer as dependencies.
    Update gatsby-config to use the postcss-sass plugin with autoprefixer.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
+const autoprefixer = require(`autoprefixer`)
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+  	'gatsby-plugin-react-helmet',
+  	{
+			resolve: `gatsby-plugin-postcss-sass`,
+			options: {
+			postCssPlugins: [autoprefixer()],
+				precision: 8, // SASS default: 5
+			},
+		},
+  ],
 };
