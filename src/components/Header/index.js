6c9f8533fae2abb0d8045eb24a4cbe92ebb0db66
jacobvenable <jacob.venable@gmail.com>
2018-03-31 22:43:57 -0600

    Update header component HTML.
    Fix missing SASS mixin names. Update media queries to output appropriately.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,32 +2,23 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+  <header className="header">
+    <div className="container">
+      <a href="index.html" className="header__link header__link-svg">
+        <object data="images/logo.svg" type="image/svg+xml" className="header__logo header__logo-svg">
+          <img src="images/logo.png" alt="logo" className="header__logo"/>
+        </object>
+      </a>
+      <h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>
+      <nav className="nav nav-main" role="navigation">
+        <ul className="nav__list">
+          <li className="nav__item"><a href="#" className="nav__link nav__link-main">About</a></li>
+          <li className="nav__item"><a href="#" className="nav__link nav__link-main">Work</a></li>
+          <li className="nav__item"><a href="#" className="nav__link nav__link-main">Contact</a></li>
+        </ul>
+      </nav>
     </div>
-  </div>
+  </header>
 )
 
 export default Header
