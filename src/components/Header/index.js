9d08f8fe96f841d93eea8f628d783cdd13c8d920
jacobvenable <jacob.venable@gmail.com>
2018-04-11 21:55:59 -0600

    Update the header index.js to use the header JSX.
    Rename the page-2.js to typography.js and update JSX to display some default elements.
    
    Update index.js layout to place the container around the content.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const Header = () => (
   <header className="header">
     <div className="container">
-      <a href="index.html" className="header__link header__link-svg">
+      <a href="/index" className="header__link header__link-svg">
         <object data="images/logo.svg" type="image/svg+xml" className="header__logo header__logo-svg">
           <img src="images/logo.png" alt="logo" className="header__logo"/>
         </object>
       </a>
-      <h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>
+      {/*<h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>*/}
       <nav className="nav nav-main" role="navigation">
         <ul className="nav__list">
           <li className="nav__item"><a href="#" className="nav__link nav__link-main">About</a></li>
@@ -19,6 +19,6 @@ const Header = () => (
       </nav>
     </div>
   </header>
-)
+);
 
-export default Header
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import '../scss/all.scss';
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+  <main id="main">
+    <div className="container">
+	    <h1>Hi people</h1>
+	    <p>Welcome to your new Gatsby site.</p>
+	    <p>Now go build something great.</p>
+	    <Link to="/typography/">Go to page 2</Link>
+	  </div>
+  </main>
+);
 
-export default IndexPage
+export default IndexPage;
