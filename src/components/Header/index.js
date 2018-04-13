746d0bde414327f4c7f7371f479ea885a38487a8
jacobvenable <jacob.venable@gmail.com>
2018-04-12 18:06:49 -0600

    Import and place logo into header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import logoSvg from './../../images/logo.svg';
+import logoPng from './../../images/logo.png';
+
 const Header = () => (
   <header className="header">
     <div className="container">
       <a href="/index" className="header__link header__link-svg">
-        <object data="images/logo.svg" type="image/svg+xml" className="header__logo header__logo-svg">
-          <img src="images/logo.png" alt="logo" className="header__logo"/>
+        <object data={logoSvg} type="image/svg+xml" className="header__logo header__logo-svg">
+          <img src={logoPng} alt="logo" className="header__logo"/>
         </object>
       </a>
       {/*<h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>*/}
