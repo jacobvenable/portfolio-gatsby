00d5cbf770cdf17b544b0b40ae3e53e9401b3a6c
jacobvenable <jacob.venable@gmail.com>
2018-04-17 22:06:19 -0600

    Fix the logo link within the header to properly span the entire logo.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,12 +7,14 @@ import logoPng from './../../images/logo.png';
 const Header = () => (
   <header className="header">
     <div className="container">
-      <Link to="/index/" className="header__link header__link-svg">
-        <object data={logoSvg} type="image/svg+xml" className="header__logo header__logo-svg" tabIndex="-1">
-          <img src={logoPng} alt="logo" className="header__logo"/>
-        </object>
+      <Link to="/index/" className="header__link header__link-logo">
+        <div className="svg header__logo">
+          <object data={logoSvg} type="image/svg+xml" className="svg__object" tabIndex="-1">
+            <img src={logoPng} alt="logo" className="header__logo"/>
+          </object>
+        </div>
       </Link>
-      {/*<h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>*/}
+      <h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>
       <nav className="nav nav-main" role="navigation">
         <ul className="nav__list">
           <li className="nav__item"><a href="#" className="nav__link nav__link-main">About</a></li>
