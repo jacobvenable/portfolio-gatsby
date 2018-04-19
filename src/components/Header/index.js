121d36c9c9cbed643928cec54df9d7f7af644889
jacobvenable <jacob.venable@gmail.com>
2018-04-18 21:23:39 -0600

    Style the heading, name, and title of the header.
    Create new sr.scss to contain styles for screen readers and include it within all the objects SCSS partials.
    
    Update markup in header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,16 +6,21 @@ import logoPng from './../../images/logo.png';
 
 const Header = () => (
   <header className="header">
-    <div className="container">
-      <Link to="/index/" className="header__link header__link-logo">
+    <div className="container header__container">
+      <Link to="/" className="header__link header__link-logo">
+        <span className="sr-hidden">Home</span> 
         <div className="svg header__logo">
           <object data={logoSvg} type="image/svg+xml" className="svg__object" tabIndex="-1">
             <img src={logoPng} alt="logo" className="header__logo"/>
           </object>
         </div>
       </Link>
-      <h1 className="header__h1">Jacob Venable &#8212; Front-end Web Developer</h1>
-      <nav className="nav nav-main" role="navigation">
+      <h1 className="header__heading">
+        <span className="header__name">Jacob Venable </span>
+        <span className="sr-hidden">&#8212;</span> 
+        <span className="header__title">Front-End Web Developer</span>
+      </h1>
+      <nav className="header__nav nav nav-main" role="navigation">
         <ul className="nav__list">
           <li className="nav__item"><a href="#" className="nav__link nav__link-main">About</a></li>
           <li className="nav__item"><Link to="/work/" className="nav__link nav__link-main">Work</Link></li>
