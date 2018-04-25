ba6fcc2b14299a854895e203bc2bdcb48ca6cc05
jacobvenable <jacob.venable@gmail.com>
2018-04-24 22:16:49 -0600

    Complete mobile version of header and navigation.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Link from 'gatsby-link';
 import StickyFill from 'stickyfilljs';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faBars from '@fortawesome/fontawesome-pro-solid/faBars';
+import faHome from '@fortawesome/fontawesome-pro-solid/faHome';
+import faTimes from '@fortawesome/fontawesome-pro-light/faTimes';
+import faUserAlt from '@fortawesome/fontawesome-pro-solid/faUserAlt';
+import faFolderOpen from '@fortawesome/fontawesome-pro-solid/faFolderOpen';
+import faEnvelope from '@fortawesome/fontawesome-pro-solid/faEnvelope';
+
+
 import logoSvg from './../../images/logo.svg';
 import logoPng from './../../images/logo.png';
 
@@ -12,8 +21,13 @@ class Header extends React.Component {
     this.state = {
       checkingStick:false,
       sticky:false,
-      stickyClass:""
+      stickyClass:"",
+      mobileNavOpen:false,
+      mobileNavOpenClass:"",
+      togglerIcon:faBars,
+      togglerText:"open navigation"
     };
+    this.toggleOpen = this.toggleOpen.bind(this);
   }
 
   componentDidMount() {
@@ -39,8 +53,17 @@ class Header extends React.Component {
   toggleSticky(){
     this.setState({
       sticky:!this.state.sticky,
-      stickyClass:(!this.state.sticky)?" header-sticking":""
+      stickyClass:!this.state.sticky?" header-sticking":""
     });
+  }
+
+  toggleOpen(){
+    this.setState({
+      mobileNavOpen:!this.state.mobileNavOpen,
+      mobileNavOpenClass:!this.state.mobileNavOpen?" nav-main-open":"",
+      togglerIcon:!this.state.mobileNavOpen?faTimes:faBars,
+      togglerText:!this.state.mobileNavOpen?"close navigation":"open navigation"
+    })
   }
 
   render(){
@@ -60,11 +83,38 @@ class Header extends React.Component {
             <span className="sr-hidden">&#8212;</span> 
             <span className="header__title">Front-End Web Developer</span>
           </h1>
-          <nav className="header__nav nav nav-main" role="navigation">
+          <nav className={`header__nav nav nav-main${this.state.mobileNavOpenClass}`} role="navigation">
+            <div className="container nav__container nav__container-toggler">
+              <button className="nav__toggler nav__toggler-main" onClick={this.toggleOpen}>
+                <FontAwesomeIcon fixedWidth icon={this.state.togglerIcon} className="nav__icon nav__icon-toggler" />
+                <span className="sr-only">{`${this.state.togglerText}`}</span>
+              </button>
+            </div>
             <ul className="nav__list nav__list-main">
-              <li className="nav__item nav__item-main"><a href="#" className="nav__link nav__link-main">About</a></li>
-              <li className="nav__item nav__item-main"><Link to="/work/" className="nav__link nav__link-main">Work</Link></li>
-              <li className="nav__item nav__item-main"><a href="#" className="nav__link nav__link-main">Contact</a></li>
+              <li className="nav__item nav__item-main nav__item-home">
+                <Link to="/" className="nav__link nav__link-main">
+                  <FontAwesomeIcon icon={faHome} className="nav__icon nav__icon-main"/>
+                  Home
+                </Link>
+              </li>
+              <li className="nav__item nav__item-main">
+                <a href="#" className="nav__link nav__link-main">
+                  <FontAwesomeIcon icon={faUserAlt} className="nav__icon nav__icon-main"/>
+                  About
+                </a>
+              </li>
+              <li className="nav__item nav__item-main">
+                <Link to="/work/" className="nav__link nav__link-main">
+                  <FontAwesomeIcon icon={faFolderOpen} className="nav__icon nav__icon-main" />
+                  Work
+                </Link>
+              </li>
+              <li className="nav__item nav__item-main">
+                <a href="#" className="nav__link nav__link-main">
+                  <FontAwesomeIcon icon={faEnvelope} className="nav__icon nav__icon-main" />
+                  Contact
+                </a>
+              </li>
             </ul>
           </nav>
         </div>
