cecd1d9b13961192fe7aa4c599a46565f1b17d44
jacobvenable <jacob.venable@gmail.com>
2018-04-24 22:26:07 -0600

    Change class name on portfolio item role label to "sr-only".

--- src/components/PortfolioItem/index.js
+++ src/components/PortfolioItem/index.js
@@ -6,7 +6,7 @@ const PortfolioItem = props => {
   return <section aria-labelledby={`portfolio__title-${props.index}`} className={`portfolio__item${props.index % 2 != 0 ? " portfolio__item-reverse" : ""}`}>
     <div className={`portfolio__container portfolio__container-details${props.index % 2 != 0 ? " portfolio__container-details-reverse" : ""}`}>
       <h2 id={`portfolio__title-${props.index}`} className="portfolio__title portfolio__title-item">{props.title}</h2>
-      <p className="portfolio__role portfolio__role-item"><span className="portfolio__role-sr">My Role: </span>{props.role}</p>
+      <p className="portfolio__role portfolio__role-item"><span className="sr-only">My Role: </span>{props.role}</p>
     </div>
     <div className={`portfolio__container portfolio__container-thumb${props.index % 2 != 0 ? " portfolio__container-thumb-reverse" : ""}`}>
       {<Img sizes={props.thumb.node.sizes} className="portfolio__image portfolio__image-item"/>}
