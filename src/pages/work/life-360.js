6e5948b730617091e1ab29df49c943d60fe611d5
jacobvenable <jacob.venable@gmail.com>
2018-06-28 09:20:09 -0600

    Update font awesome and add styles for h3 elements.
    Add content to the life-360 portfolio item.

--- src/pages/work/life-360.js
+++ src/pages/work/life-360.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faClipboardList } from '@fortawesome/fontawesome-pro-solid';
+import { faToolbox } from '@fortawesome/fontawesome-pro-solid';
 
 exports.frontmatter = {
     title: 'Life 360',
@@ -8,23 +11,24 @@ exports.frontmatter = {
     blurb: 'Custom-built magazine site to showcase people, places and events from across the College of Health and Human Sciences.',
     thumb: 'portfolio_life360-thumb.jpg',
     date: '2016-11-01',
-    tech: ['GulpJS','HTML5','PostCSS','cssnext','Vanilla JS']
+    tech: ['GulpJS','HTML5','PostCSS','cssnext','Vanilla JS','Lazy Loading']
 }
 
 const Life360Page = ({data}) => (
   <div className="container">
-		<h1>Life 360</h1>
-		<p>Life 360 is an annual print and online magazine released by the Purdue College of Health and Human Sciences (HHS) featuring stories highlighting students, staff, faculty, and alumni of HHS.</p>
+		<h1 aria-describedby="intro">Life 360</h1>
+		<p className="intro" id="intro">The online magazine of the Purdue College of Health and Human Sciences (HHS)</p>
 		<h2>Overview</h2>
-		<h3>Responsibilities</h3>
+		<h3><FontAwesomeIcon icon={faClipboardList} className=""/>Responsibilities</h3>
 		<ul>
 			<li>review design for accessibility issues</li>
 			<li>develop reuseable components and templates based on design</li>
 			<li>test site for unintended browser inconsistencies</li>
+			<li>implement the the site within a CMS for easy management</li>
 		</ul>
-		<h3>Tech</h3>
+		<h3><FontAwesomeIcon icon={faToolbox} className=""/>Tech</h3>
 		<dl>
-			<dt>Worflow Manager</dt>
+			<dt>Task Runner</dt>
 			<dd><a href="https://gulpjs.com/">Gulp.js</a></dd>
 			<dt>CSS</dt>
 			<dd>Structured with <a href="http://getbem.com/">BEM</a> and processed by <a href="http://cssnext.io/">PostCSS-cssnext</a></dd>
