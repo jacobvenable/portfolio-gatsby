b3a9294bedd3f22eb66e25a32f0511e437ba5bfc
jacobvenable <jacob.venable@gmail.com>
2018-08-25 16:25:44 -0600

    Add video component features and styles.
    Update video component title and button to appear and disappear correctly.
    
    Add video mask that sits over video element and title to detect mouse movements and prevent right-click downloading.
    
    Start progress bar implementation that properly updates with the video's progress.
    
    Update body generic SCSS partial to have bottom padding by default.

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -9,55 +9,111 @@ class Video extends React.Component {
 
 	constructor(props) {
     super(props);
+    this.videoMask = React.createRef();
     this.videoElement = React.createRef();
+    this.videoPlayPauseButton = React.createRef();
     this.title = props.title || null;
     this.id = props.id || null;
     this.poster = props.poster || null;
     this.mp4 = props.mp4 || null;
     this.state = {
       paused:true,
-      buttonText:'play',
-      buttonIcon:faPlay
+      controls:true,
+      progress:0
     };
-    this.toggleVideo = this.toggleVideo.bind(this);
     this.resetVideo = this.resetVideo.bind(this);
+    this.toggleVideo = this.toggleVideo.bind(this);
+    this.playVideo = this.playVideo.bind(this);
+    this.pauseVideo = this.pauseVideo.bind(this);
+    this.showControls = this.showControls.bind(this);
+    this.hideControls = this.hideControls.bind(this);
+    this.updateProgress = this.updateProgress.bind(this);
   }
 
   componentDidMount(){
     this.videoElement.current.addEventListener('ended',this.resetVideo);
+    this.videoMask.current.addEventListener('click',this.toggleVideo);
+    this.videoElement.current.addEventListener('timeupdate',this.updateProgress);
+    this.videoMask.current.addEventListener('mousemove',() => this.showControls(!this.state.paused));
+    this.videoPlayPauseButton.current.addEventListener('mousemove',() => this.showControls(!this.state.paused));
   }
 
   resetVideo(){
     this.setState({
-      buttonText:'play',
-      buttonIcon:faPlay,
       paused:true
     });
+    this.showControls();
   }
 
   toggleVideo(){
-    this.state.paused ? this.videoElement.current.play() : this.videoElement.current.pause();
+    console.log('toggle');
+    this.state.paused ? this.playVideo() : this.pauseVideo();
     this.setState({
-      buttonText:this.state.paused?'pause':'play',
-      buttonIcon:this.state.paused?faPause:faPlay,
       paused:!this.state.paused
     });
   }
 
-  playPauseButton(e){
+  playVideo(){
+    this.scheduleHideControls();
+    this.videoElement.current.play();
+  }
+
+  pauseVideo(){
+    this.videoElement.current.pause();
+    this.showControls();
+  }
+
+  showControls(rescheduleHideControls = false){
+    if(!this.state.controls){
+      rescheduleHideControls ? this.scheduleHideControls() : null;
+      this.setState({
+        controls:true
+      });
+    }
+  }
+
+  scheduleHideControls(){
+    typeof this.timeoutHideControls !== 'undefined' ? clearTimeout(this.timeoutHideControls) : null;
+    this.timeoutHideControls = setTimeout(() => this.hideControls(),2000);
+  }
+
+  hideControls(){
+    if(this.state.controls && !this.state.paused){
+      this.setState({
+        controls:false
+      });
+      clearTimeout(this.timeoutHideControls);
+    }
+  }
+
+  playPauseButton(){
     return(
-      <button className="video__button video__button--play" onClick={this.toggleVideo}>
-        <span className="sr-only">{this.state.buttonText}</span>
-        <FontAwesomeIcon icon={this.state.buttonIcon} className="video__icon"/>
+      <button ref={this.videoPlayPauseButton} className={`video__button video__button--play${!this.state.controls?' video__button--hidden':''}`} onClick={this.toggleVideo}>
+        <span className="sr-only">{this.state.paused?'play':'pause'}</span>
+        <FontAwesomeIcon icon={this.state.paused?faPlay:faPause} className="video__icon"/>
       </button>
+    );
+  }
+
+  updateProgress(){
+    this.setState({
+      progress:this.videoElement.current.currentTime/this.videoElement.current.duration*100
+    });
+  }
+
+  progressBar(){
+    return(
+      <progress className="video__progress" value={this.state.progress} max="100"></progress>
     );
   }
 
   render(){
     return (
-      <div className="video">
-        <p id={this.id}>{this.title}</p>
+      <div className={`video${!this.state.controls?' video--controls-hidden':''}`}>
+        <div ref={this.videoMask} className="video__mask"></div>
+        <p id={this.id} className={`video__title${!this.state.controls?' video__title--hidden':''}`}>{this.title}</p>
         {this.playPauseButton()}
+        {this.progressBar()}
         <video ref={this.videoElement} aria-labelledby={this.id} className="video__element" preload="none" poster={this.poster}>
           <source src={this.mp4} type="video/mp4"/>
         </video>
