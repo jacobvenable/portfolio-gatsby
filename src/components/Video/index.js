27dbb2d1b7bf220e977fa0cd529e0656564595f1
jacobvenable <jacob.venable@gmail.com>
2018-08-27 08:47:18 -0600

    Add blur effect to video when it hasn't been interacted with.
    Update play/pause button to have third, reset state that shows a replay icon after the video has completed playing.

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Img from 'gatsby-image';
 import generateId from './../../utils/generateId';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faVideo } from '@fortawesome/fontawesome-pro-light';
 import { faPlay } from '@fortawesome/fontawesome-pro-solid';
 import { faPause } from '@fortawesome/fontawesome-pro-solid';
+import { faRedoAlt } from '@fortawesome/fontawesome-pro-regular';
 
 class Video extends React.Component {
 
@@ -17,9 +19,11 @@ class Video extends React.Component {
     this.poster = props.poster || null;
     this.mp4 = props.mp4 || null;
     this.state = {
-      paused:true,
       controls:true,
-      progress:0
+      paused:true,
+      progress:0,
+      reset:false,
+      interaction:false
     };
     this.resetVideo = this.resetVideo.bind(this);
     this.toggleVideo = this.toggleVideo.bind(this);
@@ -40,7 +44,8 @@ class Video extends React.Component {
 
   resetVideo(){
     this.setState({
-      paused:true
+      paused:true,
+      reset:true,
     });
     this.showControls();
   }
@@ -49,7 +54,9 @@ class Video extends React.Component {
     console.log('toggle');
     this.state.paused ? this.playVideo() : this.pauseVideo();
     this.setState({
-      paused:!this.state.paused
+      paused:!this.state.paused,
+      reset:false,
+      interaction:true
     });
   }
 
@@ -90,7 +97,7 @@ class Video extends React.Component {
     return(
       <button ref={this.videoPlayPauseButton} className={`video__button video__button--play${!this.state.controls?' video__button--hidden':''}`} onClick={this.toggleVideo}>
         <span className="sr-only">{this.state.paused?'play':'pause'}</span>
-        <FontAwesomeIcon icon={this.state.paused?faPlay:faPause} className="video__icon"/>
+        <FontAwesomeIcon icon={this.state.reset?faRedoAlt:(this.state.paused?faPlay:faPause)} flip={`${this.state.reset?'horizontal':''}`} className="video__icon"/>
       </button>
     );
   }
@@ -103,7 +110,10 @@ class Video extends React.Component {
 
   progressBar(){
     return(
-      <progress className="video__progress" value={this.state.progress} max="100"></progress>
+      <div>
+        <div className={`video__progress video__progress--visible${!this.state.controls?' video__progress--hidden':''}`} style={{ width:`${this.state.progress}%` }}></div>
+        <progress className="video__progress video__progress--hidden" value={this.state.progress} max="100"></progress>
+      </div>
     );
   }
 
@@ -111,10 +121,10 @@ class Video extends React.Component {
     return (
       <div className={`video${!this.state.controls?' video--controls-hidden':''}`}>
         <div ref={this.videoMask} className="video__mask"></div>
-        <p id={this.id} className={`video__title${!this.state.controls?' video__title--hidden':''}`}>{this.title}</p>
+        <p id={this.id} className={`video__title${!this.state.controls?' video__title--hidden':''}`}><FontAwesomeIcon icon={faVideo}/> {this.title}</p>
         {this.playPauseButton()}
         {this.progressBar()}
-        <video ref={this.videoElement} aria-labelledby={this.id} className="video__element" preload="none" poster={this.poster}>
+        <video ref={this.videoElement} aria-labelledby={this.id} className={`video__element${!this.state.interaction?' video__element--initial':''}`} preload="none" poster={this.poster}>
           <source src={this.mp4} type="video/mp4"/>
         </video>
       </div>
