450fba8ccb76e8001d47d0362d982e81069dd931
jacobvenable <jacob.venable@gmail.com>
2018-10-07 13:51:24 -0600

    Remove action attribute from contact form.

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -39,7 +39,7 @@ class ContactForm extends React.Component {
 
   render(){
     return (
-      <form name="contact" onSubmit={(e) => this.handleSubmit(e)} className="container__column contact__form" action="" method="POST" netlify="true" netlify-recaptcha netlify-honeypot="sweet-honey">
+      <form name="contact" onSubmit={(e) => this.handleSubmit(e)} className="container__column contact__form" method="POST" netlify="true" netlify-recaptcha netlify-honeypot="sweet-honey">
         <div className="contact__input contact__input--detail">
           {this.inputName}
         </div>
