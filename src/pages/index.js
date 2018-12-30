41ee40bdcfabe9220cff331f24c49c5045accf45
jacobvenable <jacob.venable@gmail.com>
2018-12-30 17:53:57 -0500

    Add final questions to FAQ section

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,38 +80,42 @@ const IndexPage = () => (
 					<h2>FAQ</h2>
 					<Accordions classButtonToggle="accordion__button--open" classContent="accordion__content" classContentToggle="accordion__content--visible">
 						<button className="accordion__button" role="heading" aria-level="3">Where are you based? <FontAwesomeIcon icon={faArrowAltRight} className='accordion__icon'/></button>
-						<p>I currently live within driving distance of Boulder and Denver, Colorado. I have been working remotely for Purdue's Office of Marketing and Media, which is based in Indiana, since August 2017.</p>
+							<p>I currently live within driving distance of Boulder and Denver, Colorado. I have been working remotely for Purdue's Office of Marketing and Media, which is based in Indiana, since August 2017.</p>
 						<button className="accordion__button" role="heading" aria-level="3">What exactly is it that you do? <FontAwesomeIcon icon={faArrowAltRight} className='accordion__icon'/></button>
-						<p>In my current role as a senior front-end developer for Purdue Marketing and Media, I have a wide range of duties:</p>
-						<ul>
-							<li>create sites based on images provided by UX/UI designers (HTML, CSS, and JS)</li>
-							<li>setup sites within the univerity's content management system (Cascade Server)</li>
-							<li>train end-users in using the content management system</li>
-							<li>setup and improve the team's development workflows and standards</li>
-							<li>guide other web developers on the team</li>
-						</ul>
-						<p>In the end, I like to think of myself a problem-solver. If something doesn't work well or efficiently, I like to find out how to make it better, and it shows with the changes I've made while at Purdue:</p>
-						<ul>
-							<li>implemented Gulp.js to automate the development workflow</li>
-							<li>added SCSS to our toolket to improve styles management</li>
-							<li>created reusable patterns using BEM CSS class structuring reducing CSS specificity to 1 - 2 levels</li>
-							<li>improved JS bundling to decrease page load times</li>
-							<li>instituted and documented the development team’s Git branching model creating uniformity and accountability through code reviews</li>
-							<li>developed an automatic, custom SVG parser within our CMS to eliminate developer intervention when implementing inline SVGs</li>
-							<li>initiated and developed an improved UI within our content management system</li>
+							<p>In my current role as a senior front-end developer for Purdue Marketing and Media, I have a wide range of duties:</p>
+							<ul>
+								<li>create sites based on images provided by UX/UI designers (HTML, CSS, and JS)</li>
+								<li>setup sites within the univerity's content management system (Cascade Server)</li>
+								<li>train end-users in using the content management system</li>
+								<li>setup and improve the team's development workflows and standards</li>
+								<li>guide other web developers on the team</li>
+							</ul>
+							<p>In the end, I like to think of myself a problem-solver. If something doesn't work well or efficiently, I like to find out how to make it better, and it shows with the changes I've made while at Purdue:</p>
+							<ul>
+								<li>implemented Gulp.js to automate the development workflow</li>
+								<li>added SCSS to our toolket to improve styles management</li>
+								<li>created reusable patterns using BEM CSS class structuring reducing CSS specificity to 1 - 2 levels</li>
+								<li>improved JS bundling to decrease page load times</li>
+								<li>instituted and documented the development team’s Git branching model creating uniformity and accountability through code reviews</li>
+								<li>developed an automatic, custom SVG parser within our CMS to eliminate developer intervention when implementing inline SVGs</li>
+								<li>initiated and developed an improved UI within our content management system</li>
 						</ul>
 						<button className="accordion__button" role="heading" aria-level="3">How did you get into web development? <FontAwesomeIcon icon={faArrowAltRight} className='accordion__icon'/></button>
-						<p>Since my first programming class in high school, I've known that I've wanted to do some kind of development as my career. To move in that direction, I applied to and was accepted into Purdue University's Computer Science program.</p>
-						<p>After a year in the program I learned there was a caveat to my career choice; it needed to be visually-rewarding. While the Computer Science program gave me a great foundation, I felt I was missing out on applying it. I looked around for something a bit more practical and decided to take an introduction to web development, which was part of the Computer Graphics Technology (CGT) program.</p>
-						<p>Long story short, I fell in love with web development, switched to the CGT program, and the rest is history.</p>
+							<p>Since my first programming class in high school, I've known that I've wanted to do some kind of development as my career. To move in that direction, I applied to and was accepted into Purdue University's Computer Science program.</p>
+							<p>After a year in the program I learned there was a caveat to my career choice; it needed to be visually-rewarding. While the Computer Science program gave me a great foundation, I felt I was missing out on applying it. I looked around for something a bit more practical and decided to take an introduction to web development, which was part of the Computer Graphics Technology (CGT) program.</p>
+							<p>Long story short, I fell in love with web development, switched to the CGT program, and the rest is history.</p>
 						<button className="accordion__button" role="heading" aria-level="3">What do you love about front-end development? <FontAwesomeIcon icon={faArrowAltRight} className='accordion__icon'/></button>
-						<p>During my time at Purdue, I was exposed to and practiced various skillsets involved with creating a final product for the web:</p>
-						<ul>
-							<li>UI/UX and Graphic Design</li>
-							<li>Front-End Development</li>
-							<li>Back-End Development</li>
-						</ul>
-						<p>While each of these skillsets have some overlap and are interesting in their own way, front-end development was where I found a home. It is the perfect combination of <mark>creativity</mark> and <mark>logic</mark> while also having a <mark>visually-rewarding</mark> product as the end-result.</p>
+							<p>During my time at Purdue, I was exposed to and practiced various skillsets involved with creating a final product for the web:</p>
+							<ul>
+								<li>UI/UX and Graphic Design</li>
+								<li>Front-End Development</li>
+								<li>Back-End Development</li>
+							</ul>
+							<p>While each of these skillsets have some overlap and are interesting in their own way, front-end development was where I found a home. It is the perfect combination of <mark>creativity</mark> and <mark>logic</mark> while also having a <mark>visually-rewarding</mark> product as the end-result.</p>
+						<button className="accordion__button" role="heading" aria-level="3">How do you take your coffee? <FontAwesomeIcon icon={faArrowAltRight} className='accordion__icon'/></button>
+							<p>Black&hellip; but sometimes I need to splurge on a mocha.</p>
+						<button className="accordion__button" role="heading" aria-level="3">Would you rather fight a horse-sized duck or a hundred duck-sized horses? <FontAwesomeIcon icon={faArrowAltRight} className='accordion__icon'/></button>
+							<p>I would definitely fight a horse-sized duck. Battling a hundred of anything would be tiring.</p>
 					</Accordions>
 				</Disclosure>
 			</div>
