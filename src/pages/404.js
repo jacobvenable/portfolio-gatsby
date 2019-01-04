a32d7db7eecb056ff31793187d0f64861b19634a
jacobvenable <jacob.venable@gmail.com>
2019-01-03 19:52:53 -0500

    Update 404 page content.
    Create missing SCSS component for styling elements on 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import Layout from './../components/Layout';
+import React from 'react';
+import Link from 'gatsby-link';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import './../scss/all.scss';
+
+import svg404 from './../images/404.svg';
+import png404 from './../images/404.png';
 
 const NotFoundPage = () => (
-	<Layout>
-	  <div className="container">
-	    <h1>NOT FOUND</h1>
-	    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+	<div>
+	  <div className="container missing">
+	    <h1 className="missing__heading">404 &#8212; Page Not Found</h1>
+	    <p className="missing__copy">We ain't found it!</p>
+	    <div className="missing__container missing__container--svg">
+		    <div className="svg missing__svg">
+	        <object aria-hidden="true" data={svg404} type="image/svg+xml" className="svg__object" tabIndex="-1">
+	          <img src={png404} alt="" className="header__logo"/>
+	        </object>
+	      </div>
+      </div>
+      <h2 class="sr-only">Main Pages</h2>
+      <ul className="missing__list">
+      	<li className="missing__item">
+      		<Link to='/' className='missing__link'>Home</Link>
+      	</li>
+      	<li className="missing__item">
+      		<Link to='/skills' className='missing__link'>Skills</Link>
+      	</li>
+      	<li className="missing__item">
+      		<Link to='/work' className='missing__link'>Work</Link>
+      	</li>
+      	<li className="missing__item">
+      		<Link to='/contact' className='missing__link'>Contact</Link>
+      	</li>
+      </ul>
 	  </div>
-	</Layout>
+	</div>
 )
 
 export default NotFoundPage
