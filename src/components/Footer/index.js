7e14d6d2719411965ebe49cb302c2dba6734d9e3
jacobvenable <jacob.venable@gmail.com>
2019-01-07 17:27:42 -0500

    Fix improper class attribute to className in footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   let today = new Date();
   return (
-    <footer class="footer">
+    <footer className="footer">
       <div className="container">
         <p className="footer__copy">Copyright &copy; {today.getFullYear()} Jacob Venable</p>
       </div>
