e185757102921df1032794cb6015b598611cc073
Jacob <jacob.venable@gmail.com>
2019-01-11 14:16:58 -0700

    Reorder component for non-grid browsers. Change portfolio item from a block to flex element at mobile. Use flex ordering to reorder elements at mobile

--- src/components/PortfolioItem/index.js
+++ src/components/PortfolioItem/index.js
@@ -23,15 +23,15 @@ class PortfolioItem extends React.Component {
           <h2 id={`portfolio__title-${this.index}`} className="portfolio__title portfolio__title-item">{this.title}</h2>
           <p className="portfolio__role portfolio__role-item"><span className="sr-only">My Role: </span>{this.role}</p>
         </div>
+        <div className={`portfolio__container portfolio__container-thumb${this.index % 2 !== 0 ? " portfolio__container-thumb-reverse" : ""}`}>
+          <Img fluid={this.thumb.node.fluid} className="portfolio__image portfolio__image-item"/>
+        </div>
         <div className={`portfolio__container portfolio__container-tech${this.index % 2 !== 0 ? " portfolio__container-tech-reverse" : ""}`}>
           {this.tech.length > 0 && 
             <ul className="tags">
               {this.tech.map(tag => <li key={tag.toString()} className="tags__tag">{tag}</li>)}
             </ul>
           }
-        </div>
-        <div className={`portfolio__container portfolio__container-thumb${this.index % 2 !== 0 ? " portfolio__container-thumb-reverse" : ""}`}>
-          <Img fluid={this.thumb.node.fluid} className="portfolio__image portfolio__image-item"/>
         </div>
         <div className={`portfolio__container portfolio__container-summary${this.index % 2 !== 0 ? " portfolio__container-summary-reverse" : ""}`}>
           <p className="portfolio__description portfolio__description-item">{this.blurb}</p>
