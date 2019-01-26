f6de686bb40fdf3b6695c7bafa4c07cfe311b1c8
jacobvenable <jacob.venable@gmail.com>
2019-01-25 22:41:33 -0700

    Change Figure component into a function component.
    Add comments to the Figure component.

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import Img from 'gatsby-image';
-import generateId from './../utils/generateId';
 
-class Figure extends React.Component {
-
-	constructor(props) {
-    super(props);
-    this.full = props.full || false;
-    this.fluid = props.fluid;
-    this.alt = props.alt || '';
-    this.caption = props.caption || false;
-    this.id = generateId('figure');
-  }
-
-  render(){
-    return (
-      <figure className={`figure${this.full?' figure--full':''}`}>
-				<Img fluid={this.fluid} className="figure__img" alt={this.alt}/>
-				{ typeof this.caption === 'string' &&
-					<figcaption className="figure__caption">{this.caption}</figcaption>
-				}
-			</figure>
-    );
-  }
-
+/**
+ * A functional React component used to place a figure, an image and (optional) caption, on a page.
+ * @param {boolean} props.full - determines if 'full' modifier class should be applied to the figure
+ * @param {object} props.fluid - an image sharp object returned from a GraphQl query used to create responsive images
+ * @param {string} props.alt - optional string containing the alt attribute of the image
+ * @param {string} props.caption - optional string containing the visible caption placed below the image
+ */
+function Figure(props){
+  return (
+    <figure className={`
+      figure
+      ${props.full?'figure--full':''}`
+    }>
+			<Img fluid={props.fluid} className="figure__img" alt={props.alt || ''}/>
+			{ typeof props.caption === 'string' &&
+				<figcaption className="figure__caption">{props.caption}</figcaption>
+			}
+		</figure>
+  );
 }
 
 export default Figure;
