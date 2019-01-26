7b6ab449fad9c76dee3341d3c900fb8f56ab71dc
jacobvenable <jacob.venable@gmail.com>
2019-01-26 01:17:31 -0700

    Convert PortfolioItems to functional component

--- src/components/PortfolioItems.js
+++ src/components/PortfolioItems.js
@@ -1,37 +1,34 @@
 import React from 'react';
 import PortfolioItem from './PortfolioItem';
 
-class PortfolioItems extends React.Component {
-
-  render(){
-    if(typeof this.props.data === 'object' && typeof this.props.data.allJavascriptFrontmatter === 'object' && this.props.data.allJavascriptFrontmatter !== null && this.props.data.allJavascriptFrontmatter.edges.length > 0){
-      return(
-        <div>
-          {this.props.data.allJavascriptFrontmatter.edges.map((page,index) => {
-            return(
-              <PortfolioItem 
-                key={page.node.id} 
-                relativeDirectory={page.node.node.relativeDirectory} 
-                name={page.node.node.name} 
-                title={page.node.frontmatter.title} 
-                role={page.node.frontmatter.role} 
-                blurb={page.node.frontmatter.blurb} 
-                thumb={
-                  this.props.data.thumbs.edges.find((thumb) => {
-                    return thumb.node.fluid.originalName === page.node.frontmatter.thumb
-                  })
-                }
-                tech={page.node.frontmatter.tech} 
-                index={index} 
-              />
-            );
-          })}
-        </div>
-      );
-    }
-    else{
-      return null;
-    }
+function PortfolioItems(props){
+  if(typeof props.data === 'object' && typeof props.data.allJavascriptFrontmatter === 'object' && props.data.allJavascriptFrontmatter !== null && props.data.allJavascriptFrontmatter.edges.length > 0){
+    return(
+      <div>
+        {props.data.allJavascriptFrontmatter.edges.map((page,index) => {
+          return(
+            <PortfolioItem 
+              key={page.node.id} 
+              relativeDirectory={page.node.node.relativeDirectory} 
+              name={page.node.node.name} 
+              title={page.node.frontmatter.title} 
+              role={page.node.frontmatter.role} 
+              blurb={page.node.frontmatter.blurb} 
+              thumb={
+                props.data.thumbs.edges.find((thumb) => {
+                  return thumb.node.fluid.originalName === page.node.frontmatter.thumb
+                })
+              }
+              tech={page.node.frontmatter.tech} 
+              index={index} 
+            />
+          );
+        })}
+      </div>
+    );
+  }
+  else{
+    return null;
   }
 }
 
