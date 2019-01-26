c99e2872e940cc62fb9eac9eec9735cdf02b5d17
jacobvenable <jacob.venable@gmail.com>
2019-01-26 01:21:04 -0700

    Add comments to PortfolioItems component

--- src/components/PortfolioItems.js
+++ src/components/PortfolioItems.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PortfolioItem from './PortfolioItem';
 
+/**
+ * A functional React component used to place a list of PortfolioItem components based on passed data.
+ * @param {object} props.data - data returned from a GraphQl query containing JS frontmatter from portfolio pages.
+ */
 function PortfolioItems(props){
+  //validate the data we received is usable
   if(typeof props.data === 'object' && typeof props.data.allJavascriptFrontmatter === 'object' && props.data.allJavascriptFrontmatter !== null && props.data.allJavascriptFrontmatter.edges.length > 0){
     return(
       <div>
+        {/* loop through each page pass its information to a PortfolioItem component */}
         {props.data.allJavascriptFrontmatter.edges.map((page,index) => {
           return(
             <PortfolioItem 
