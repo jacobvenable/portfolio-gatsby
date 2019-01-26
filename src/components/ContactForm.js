10bb32d347607dc25ce8aca749b3005b0ef9e68a
jacobvenable <jacob.venable@gmail.com>
2019-01-25 22:31:40 -0700

    Add comments to ContactForm component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,21 +6,31 @@ import { faSpinnerThird } from '@fortawesome/fontawesome-pro-light';
 import { faCheck } from '@fortawesome/fontawesome-pro-regular';
 import { faExclamationTriangle } from '@fortawesome/fontawesome-pro-regular';
 
+/**
+ * A class React component used to place a contact form on a page. This would include a name, email, message, honeypot, and recaptcha input.
+ */
 class ContactForm extends React.Component {
 
+  /**
+   * Create a ContactForm component.
+   */
 	constructor(props) {
     super(props);
 
+    //create 3 separate states of the contact form that determine whether the form is submitting the form, submitted the form with success, or submitted the form with an error
     this.state = {
       submitting:false,
       submittedSuccess:false,
       submittedError:false
     };
 
+    //store the form's action attribute in a variable rather than having to extract from the DOM
     this.action = '/contact';
 
+    //create a reference that will be placed on the <form> element. Reference later used to retrieve FormData object
     this.formRef = React.createRef();
 
+    //create references for each Input component that will be added to the form to enable easier calls to methods within
     this.inputNameRef = React.createRef();
     this.inputName = <Input ref={this.inputNameRef} type="text" name="name" label="What is your name?" required={true} />;
     this.inputEmailRef = React.createRef();
@@ -31,6 +41,8 @@ class ContactForm extends React.Component {
     this.inputHoney = <Input ref={this.inputHoneyRef} type="text" name="sweet-honey" label="Leave this blank if you're human" required={false} />;
     this.inputRecaptchaRef = React.createRef();
     this.inputRecaptcha = <Input ref={this.inputRecaptchaRef} type="recaptcha" name="" label="" required={true} />;
+
+    //create an array out the inputs for use array methods
     this.inputs = [
       this.inputNameRef,
       this.inputEmailRef,
@@ -39,21 +51,35 @@ class ContactForm extends React.Component {
       this.inputRecaptchaRef
     ];
 
+    //bind this object to other ContactForm methods
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  /**
+   * Handle the submission event of the <form> element.
+   * @param {object} e - The Event object of the form submission.
+   */
   handleSubmit(e){
     e.preventDefault();
+
+    //find any inputs that are invalid (if any)
     let invalidInput = this.inputs.find(input => {return !input.current.validate('submit')});
     if(typeof invalidInput === 'undefined'){
+      //no inputs were invalid
       let contactFormData = new FormData(this.formRef.current);
       let contactFormDataSerialize = '';
+
+      //loop through each item in the form and serialize the data for a POST request
       for(let pair of contactFormData.entries()) {
         contactFormDataSerialize += `${contactFormDataSerialize.length>0?'&':''}${encodeURIComponent(pair[0])}=${encodeURIComponent(pair[1])}`;
       }
+
+      //set that the component is now submitting the data
       this.setState({
         'submitting':true
       });
+
+      //submit the data
       fetch(this.action,{
         method:'POST',
         mode:'same-origin',
@@ -66,6 +92,7 @@ class ContactForm extends React.Component {
       })
       .then(r => {
         if(typeof r === 'object' && r.ok){
+          //set that the form successfully submitted
           this.setState({
             'submitting':false,
             'submittedSuccess':true
@@ -75,7 +102,7 @@ class ContactForm extends React.Component {
           });
         }
         else{
-          console.log(r);
+          //set that the form submitted with an error
           this.setState({
             'submitting':false,
             'submittedError':true
@@ -84,6 +111,9 @@ class ContactForm extends React.Component {
       });
     }
     else{
+      //there was an invalid input
+
+      //call the focus method of the Input component that has invalid input
       invalidInput.current.focusInput(true);
     }
   }
@@ -108,7 +138,11 @@ class ContactForm extends React.Component {
         <div className="sr-only">
           <input type="hidden" name="form-name" value="contact" />
         </div>
-        <div className={`contact__input contact__input--detail ${this.state.submitting || this.state.submittedSuccess ? 'contact__input--disabled' : ''}`}>
+        <div className={`
+          contact__input
+          contact__input--detail
+          ${this.state.submitting || this.state.submittedSuccess ? 'contact__input--disabled' : ''}
+        `}>
           {this.inputName}
         </div>
         <div className={`contact__input contact__input--detail ${this.state.submitting || this.state.submittedSuccess ? 'contact__input--disabled' : ''}`}>
