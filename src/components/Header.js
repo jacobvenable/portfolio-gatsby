cb09236cb89ac2eae8346c0ce3cf0e73ef808499
jacobvenable <jacob.venable@gmail.com>
2019-01-25 23:09:07 -0700

    Remove unnecessary state properties from the Header component.
    Add comments to the Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'gatsby-link';
-//import StickyFill from 'stickyfilljs';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import faBars from '@fortawesome/fontawesome-pro-solid/faBars';
@@ -14,19 +13,22 @@ import faEnvelope from '@fortawesome/fontawesome-pro-solid/faEnvelope';
 import logoSvg from './../images/logo.svg';
 //import logoPng from './../../images/logo.png';
 
+/**
+ * A class React component used to place header of each page. The header contains the logo, site name, and site navigation.
+ */
 class Header extends React.Component {
 
   constructor(props) {
     super(props);
+
+    //set the initial state if we are checking to apply sticking, if the navigation is sticking, and if the the mobile navigation is open
     this.state = {
       checkingStick:false,
       sticky:false,
-      stickyClass:"",
-      mobileNavOpen:false,
-      mobileNavOpenClass:"",
-      togglerIcon:faBars,
-      togglerText:"open navigation"
+      mobileNavOpen:false
     };
+
+    //bind this object to class methods
     this.toggleOpen = this.toggleOpen.bind(this);
   }
 
@@ -36,6 +38,9 @@ class Header extends React.Component {
     //window.addEventListener('scroll', e => { this.checkStick() });
   }
 
+  /**
+   * A method used to check if the top navigation is sticking. NOTE: This is currently not in use due to a design choice to just fix the navigation to the top without styles changes on stick.
+   */
   checkStick(){
     if(!this.state.checkingStick)
     {
@@ -50,22 +55,27 @@ class Header extends React.Component {
     }
   }
 
+  /**
+   * A method used to toggle that the component is now sticking
+   */
   toggleSticky(){
     this.setState({
       sticky:!this.state.sticky,
-      stickyClass:!this.state.sticky?" header-sticking":""
     });
   }
 
+  /**
+   * A method used to toggle that the mobile navigation is open or closed
+   */
   toggleOpen(){
     this.setState({
-      mobileNavOpen:!this.state.mobileNavOpen,
-      mobileNavOpenClass:!this.state.mobileNavOpen?" nav-main-open":"",
-      togglerIcon:!this.state.mobileNavOpen?faTimes:faBars,
-      togglerText:!this.state.mobileNavOpen?"close navigation":"open navigation"
+      mobileNavOpen:!this.state.mobileNavOpen
     })
   }
 
+  /**
+   * A sub-functional component used to contain an individual list item in the top navigation.
+   */
   navItem(link,text,icon,home = false){
     return(
       <li className={`nav__item nav__item-main${home?' nav__item-home':''}`}>
@@ -79,7 +89,10 @@ class Header extends React.Component {
 
   render(){
     return (
-      <header ref={this.element} className={`header${this.state.stickyClass}`}>
+      <header ref={this.element} className={`
+        header
+        ${this.state.sticky?'header-sticking':''}
+      `}>
         <div className="container header__container">
           <Link to="/" className="header__link header__link-logo">
             <div to="/" className="header__container header__container-logo">
@@ -94,11 +107,16 @@ class Header extends React.Component {
               <span className="header__title">Front-End Web Developer</span>
             </div>
           </Link>
-          <nav className={`header__nav nav nav-main${this.state.mobileNavOpenClass}`} role="navigation">
+          <nav role="navigation" className={`
+            header__nav
+            nav
+            nav-main
+            ${this.state.mobileNavOpen?'nav-main-open':''}
+          `}>
             <div className="container nav__container nav__container-toggler">
               <button className="nav__toggler nav__toggler-main" onClick={this.toggleOpen}>
-                <FontAwesomeIcon fixedWidth icon={this.state.togglerIcon} className="nav__icon nav__icon-toggler" />
-                <span className="sr-only">{`${this.state.togglerText}`}</span>
+                <FontAwesomeIcon fixedWidth icon={this.state.mobileNavOpen?faTimes:faBars} className="nav__icon nav__icon-toggler" />
+                <span className="sr-only">{this.state.mobileNavOpen?'close navigation':'open navigation'}</span>
               </button>
             </div>
             <ul className="nav__list nav__list-main">
