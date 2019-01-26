b7eb8d6238cbd289fb505660ca3b1ed23ae5db2e
jacobvenable <jacob.venable@gmail.com>
2019-01-26 01:22:52 -0700

    Add comments to SkipToMain component

--- src/components/SkipToMain.js
+++ src/components/SkipToMain.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const SkipToMain = () => (
+/**
+ * A functional React component returning the JSX of the "skip main content" link.
+ */
+function SkipToMain(){
+	return(
   <a href="#main" className="nav__link nav__link-skipto">Skip to main content</a>
-);
+  );
+}
 
 export default SkipToMain;
