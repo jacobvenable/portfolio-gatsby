163ab6e2c6c89243128e76fefa057d87a7fbd92f
jacobvenable <jacob.venable@gmail.com>
2019-01-26 19:36:39 -0700

    Change Disclosure component 'toggle' method to update it's 'open' property based on the passed 'open' parameter instead of ignoring the parameter.
    Add comments to Disclosure component.

--- src/components/Disclosure/index.js
+++ src/components/Disclosure/index.js
@@ -1,54 +1,74 @@
 import React from 'react';
-// import Link from 'gatsby-link';
-// import Img from 'gatsby-image';
 import DisclosureButton from './DisclosureButton';
 import DisclosureContent from './DisclosureContent';
 import generateId from './../../utils/generateId';
 
+/**
+ * A class React component used for adding disclosures according to ARIA best practices: https://www.w3.org/TR/wai-aria-practices-1.1/#disclosure
+ */
 class Disclosure extends React.Component {
 
   constructor(props){
     super(props);
 
-    //bind component to methods
-    this.toggle = this.toggle.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-
+    //create property used for instantly setting whether the disclosure is closed
     this.open = false;
 
+    //add 'open' (above) to state as well as whether the DisclosureButton has been removed (hidden) from the user. 'removed' is only used in discolsures that are single-use
     this.state = {
       open:this.open,
       removed:false
     };
 
+    //create references for DisclosureButton and DisclosureContent components for easier access to their methods
     this.buttonRef = React.createRef();
     this.contentRef = React.createRef();
 
     if(Array.isArray(this.props.children)){
       //there is more than one child within this element
+
+      //determine if there is at least one button in the children using isButton method
       let button = this.props.children.find(this.isButton);
       if(typeof button !== 'undefined'){
-        this.disclosureButton = <DisclosureButton ref={this.buttonRef} key={generateId('disclosure__button')} element={button} toggleHandler={this.toggle} classButtonToggle={this.props.classButtonToggle} classButtonBlur={this.props.classButtonBlur} {...button.props}>{button.props.children}</DisclosureButton>;
+        //there is at least one button within children so create a DisclosureButton component using that element
+        this.disclosureButton = (
+          <DisclosureButton
+            ref={this.buttonRef}
+            key={generateId('disclosure__button')}
+            element={button}
+            toggleHandler={this.toggle}
+            classButtonToggle={this.props.classButtonToggle}
+            classButtonBlur={this.props.classButtonBlur} {...button.props}>{button.props.children}
+          </DisclosureButton>
+        );
       }
     }
+
+    //bind this component to methods
+    this.toggle = this.toggle.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
+  /**
+   * Method used to determine if a passed element is a button. A button could be a <button> element or any element that has the attribute-value pair `role="button"`.
+   * @param {object} element - the element to be identified as a button
+   * @return {boolean} identifying if the element is a button (true) or not (false)
+   */
   isButton(element){
     return (element.type === 'button' || (typeof element.props.role === 'string' && element.props.role === 'button'));
   }
 
+  /**
+   * Method used to open/close the disclosure based on it's current open state.
+   * @param {boolean} open - (optional) boolean identifying what the new state of the button should be
+   */
   toggle(open = undefined){
-    this.open = !this.open;
-    this.contentRef.current.toggleOpen(open);
-  }
-
-  componentDidMount(){
-  }
-
-  handleBlur(){
+    this.open = (typeof boolean === 'boolean' ? open : !this.open);
+    this.contentRef.current.toggleOpen(this.open);
   }
 
   render(){
+    //if there is a disclosure button, render that and the rest of the non-button children within a DisclosureContent component. Otherwise, render only the children passed
     if(this.disclosureButton){
       return(
         <React.Fragment key={generateId('disclosure')}>
